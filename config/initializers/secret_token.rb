c65e7ee5465ad93cce1ed177587993dd33e60ae4
Ashish Upadhyay <ashish.cse2010@gmail.com>
2012-03-17 02:45:48 +0530

    setting up the application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evite::Application.config.secret_token = '3e89a3fa846fe1cde919b2dfa783a30aa0bc8698e80e0e741b348acd6006b5c51bfb57acebe2639cc7a41d0d4ab0580395610694bc05a9f6959197af3ecf9209'
